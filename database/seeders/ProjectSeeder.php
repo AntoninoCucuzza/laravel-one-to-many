a19f9d2083a1579c6c9088e857b5ef06aaffee17
AntoninoCucuzza <cucuzza.antonino@hotmail.com>
2023-11-13 17:45:00 +0100

    add random type in projectseeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\project;
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
@@ -16,10 +17,14 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker): void
     {
 
+        $types = Type::pluck('id');
 
         for ($i = 0; $i < 10; $i++) {
 
+
             $project = new project();
+
+            $project->type_id = $faker->randomElement($types);
             $project->title = $faker->city(); /* da cambiare */
             $project->slug = Str::slug($project->title, '-');
             $project->description = $faker->realText(200);
